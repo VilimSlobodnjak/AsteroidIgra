5eed75f0ab51f1fe99ecfce821d7964d79e1609d
viliam321 <vilims007@gmail.com>
2025-06-26 15:31:32 +0200

    Ekran na 60 fps

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from constants import *
 
 def main():
     pygame.init()
+    sat = pygame.time.Clock()
+    dt = 0
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        sat.tick(60)
+        dt = sat.tick(60)/1000
 
 
 if __name__ == "__main__":
