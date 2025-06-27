63ce76b06772bb99303d26ddfa878fcbcfcb0b72
viliam321 <vilims007@gmail.com>
2025-06-27 10:52:29 +0200

    pocetak okretanja lika

--- player.py
+++ player.py
@@ -17,3 +17,14 @@ class Player(CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+        def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+        if keys[pygame.K_d]:
+            # ?
